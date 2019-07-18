91e9a5647afda1cfca9980c6592965b1b72eb355
Ghost Ghost <ghost77@Ghosts-MBP.hsd1.ca.comcast.net>
2019-07-17 22:06:15 -0700

    added listen port

--- server.js
+++ server.js
@@ -5,3 +5,18 @@ var mongoose = require("mongoose");
 // Scrapping tools
 var axios = require("axios");
 var cheerio = require("cheerio");
+
+
+
+
+
+
+
+
+
+
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+})
